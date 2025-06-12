3b09d72e6e12b776df65ea51d68e48378dbb0197
Bob Karemera Shema <bobshema14@gmail.com>
2025-06-12 16:51:30 +0200

    feat: style MovieCard using MUI

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,12 +1,21 @@
+import { Card, CardMedia, CardContent, Typography } from "@mui/material";
+
 interface Props {
     movie: { title: string; poster_path: string };
 }
 
 const MovieCard = ({ movie }: Props) => (
-    <div>
-        <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title} />
-        <p>{movie.title}</p>
-    </div>
+    <Card sx={{ maxWidth: 200, margin: 1 }}>
+        <CardMedia
+            component="img"
+            height="300"
+            image={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+            alt={movie.title}
+        />
+        <CardContent>
+            <Typography variant="subtitle1">{movie.title}</Typography>
+        </CardContent>
+    </Card>
 );
 
 export default MovieCard;
