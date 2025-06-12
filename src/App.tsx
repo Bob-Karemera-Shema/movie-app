1546ff205b5f7fecb45f53056c6e6483f5e3bea8
Bob Karemera Shema <bobshema14@gmail.com>
2025-06-12 16:32:06 +0200

    feat: added react-router with routes for home and movie detail pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import MovieList from "./pages/MovieList";
+import MovieDetail from "./pages/MovieDetail";
 
 function App() {
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MovieList />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
